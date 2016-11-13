f0349923a65638700faddbec69f626c4a0d33533
ryanbabida <ryan.babida@gmail.com>
2016-11-12 17:23:19 -0800

    dockerized app

--- helloworld/helloworld/settings.py
+++ helloworld/helloworld/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'tjj(nqf=(hqz06d24t#xgoz$tue@p$#f3j*m6rz17f6yy^x5zn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'helloworld.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
