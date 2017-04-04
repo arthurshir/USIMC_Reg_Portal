8a4e5be2097897823e72902899234e21f90be6f2
Ubuntu <ubuntu@ip-172-31-1-128.us-west-1.compute.internal>
2017-04-04 03:36:32 +0000

    Updated uwsgi .ini file and wsgi.py to work

--- usimc_2017/usimc_2017/wsgi.py
+++ usimc_2017/usimc_2017/wsgi.py
@@ -9,7 +9,12 @@ https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
 
 import os
 
+import sys
+
 from django.core.wsgi import get_wsgi_application
+
+BASE_DIR = os.path.dirname( os.path.dirname( os.path.abspath(__file__) ) )
+sys.path.append(BASE_DIR)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "usimc_2017.settings")
 
