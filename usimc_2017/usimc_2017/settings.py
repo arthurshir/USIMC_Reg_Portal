530e3fa3bbcb9a70b29c8993b39dba85c581fc89
Ubuntu <ubuntu@ip-172-31-1-128.us-west-1.compute.internal>
2017-04-17 03:10:38 +0000

    Adjusted allowed hosts temporarily to all

--- usimc_2017/usimc_2017/settings.py
+++ usimc_2017/usimc_2017/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '123456')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'usimc2017registration']
+ALLOWED_HOSTS = ['*']
+#ALLOWED_HOSTS = ['localhost', '54.193.10.208', '127.0.0.1', 'usimc2017registration']
 
 print "DEBUG=", "True" if DEBUG else "False"
 
