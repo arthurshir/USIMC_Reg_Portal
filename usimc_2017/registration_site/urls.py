a67dfc9d959e63c18027c17e73b403623151a9e1
fantaandcrackers <arthur.shir@gmail.com>
2017-06-02 04:18:28 -0700

    Disabled registration for website and added message

--- usimc_2017/registration_site/urls.py
+++ usimc_2017/registration_site/urls.py
@@ -3,6 +3,7 @@
 from django.conf.urls import include, url
 from django.contrib.auth import views as auth_views
 from django.contrib import admin
+from django.views.generic import TemplateView
 from . import views
 
 app_name = 'registration_site'
@@ -16,24 +17,26 @@ urlpatterns = [
     url(r'^logout/', views.LogoutView.as_view(), name='logout'),
     # Application Management
     url(r'^dashboard/$', views.ApplicationListView.as_view(), name='dashboard'),
-    url(r'^dashboard/new/', views.NewApplicationView.as_view(), name='new_application'),
+    # url(r'^dashboard/new/', views.NewApplicationView.as_view(), name='new_application'),
     url(r'^dashboard/list/', views.ApplicationListView.as_view(), name='application_list'),
     # Application Submission
-    url(r'^dashboard/(?P<pk>\d+)/part2/$', views.ApplicationPart2View.as_view(), name='application_part_2'),
-    url(r'^dashboard/submission/review/(?P<pk>\d+)/$', views.ReviewSubmissionView.as_view(), name='review_submission'),
-    url(r'^dashboard/submission/pay/(?P<pk>\d+)/$', views.PaymentView.as_view(), name='pay'),
-    url(r'^dashboard/submission/confirmation/(?P<pk>\d+)/$', views.payment_confirmation, name='payment_confirmation'),
+    # url(r'^dashboard/(?P<pk>\d+)/part2/$', views.ApplicationPart2View.as_view(), name='application_part_2'),
+    # url(r'^dashboard/submission/review/(?P<pk>\d+)/$', views.ReviewSubmissionView.as_view(), name='review_submission'),
+    # url(r'^dashboard/submission/pay/(?P<pk>\d+)/$', views.PaymentView.as_view(), name='pay'),
+    # url(r'^dashboard/submission/confirmation/(?P<pk>\d+)/$', views.payment_confirmation, name='payment_confirmation'),
     # Application REST
-    url(r'^application/delete/(?P<pk>\d+)/$', views.DeleteApplicationView.as_view(), name='delete_application'),
-    url(r'^application/create_pricing_string/$', views.create_pricing_string, name='create_pricing_string'),
+    # url(r'^application/delete/(?P<pk>\d+)/$', views.DeleteApplicationView.as_view(), name='delete_application'),
+    # url(r'^application/create_pricing_string/$', views.create_pricing_string, name='create_pricing_string'),
     # Application Edit
     # url(r'^dashboard/(?P<pk>\d+)/edit/$', views.EditApplicationView.as_view(), name='edit_application'),
+    # Ending Message
+    url(r'^competition_ended/', TemplateView.as_view(template_name='registration_site/application_management/ending_message.html'), name='competition_ended'),
 
     # Admin Users
-    url(r'^admin/login/', views.AdminLoginView.as_view(), name='admin_register'),
-    url(r'^admin/download-entries/', views.DownloadEntriesView.as_view(), name='download_entries'),
-    url(r'^admin/dashboard/', views.DownloadEntriesView.as_view(), name='admin_dashboard'),
+    # url(r'^admin/login/', views.AdminLoginView.as_view(), name='admin_register'),
+    # url(r'^admin/download-entries/', views.DownloadEntriesView.as_view(), name='download_entries'),
+    # url(r'^admin/dashboard/', views.DownloadEntriesView.as_view(), name='admin_dashboard'),
 
     # Info
-    url(r'^info/rules.json$', views.rules_json, name='rules_json'),
+    # url(r'^info/rules.json$', views.rules_json, name='rules_json'),
 ]
