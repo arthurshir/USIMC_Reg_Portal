29fa73ce85ed59d5ee64d7e10c548cf367d23e21
Arthurshir <Arthur.shir@gmail.com>
2019-04-04 22:31:49 -0700

    Add missing sites package

--- usimc/usimc/settings.py
+++ usimc/usimc/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
     'registration_site',
     'crispy_forms',
     'django_excel',
